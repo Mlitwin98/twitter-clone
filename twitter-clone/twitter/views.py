711bb3793f819e19f9b70024977fd939747c5061
Mateusz Litwin <m_litwin@wp.pl>
2020-09-21 16:08:43 +0200

    Added main page

--- twitter-clone/twitter/views.py
+++ twitter-clone/twitter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+def main(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return HttpResponse('Login')
+
+def register(request):
+    return HttpResponse('Register')

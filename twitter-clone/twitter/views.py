784ae271e47cb658914d5456c73b4269620b7a46
Mateusz Litwin <m_litwin@wp.pl>
2020-09-23 16:29:30 +0200

    Added register and login page

--- twitter-clone/twitter/views.py
+++ twitter-clone/twitter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http.response import HttpResponse
 
 # Create your views here.
@@ -6,7 +6,19 @@ def main(request):
     return render(request, 'index.html')
 
 def login(request):
-    return HttpResponse('Login')
+    if request.method == 'POST':
+        if 'login' in request.POST:
+            return HttpResponse('login')
+        elif 'cancel' in request.POST:
+            return redirect('main')
+    else:
+        return render(request, 'login.html')
 
 def register(request):
-    return HttpResponse('Register')
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            return HttpResponse('register')
+        elif 'cancel' in request.POST:
+            return redirect('main')
+    else:
+        return render(request, 'register.html')
